a1786cbcb2af910540213a56685d835eefd69082
anupsawant <anup.sawant@northwestern.edu>
2016-09-02 12:17:22 -0500

    Correction in functional tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -43,9 +43,9 @@ class NewVisitorTest(LiveServerTestCase):
 		# lists "1: Buy peacock feathers" as an item in a to-do list
 		inputbox.send_keys(Keys.ENTER)
 
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		edith_list_url = self.browser.current_url
+		self.assertRegex(edith_list_url,'/lists/.+')
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 		# There is still a text box inviting her to add another item.
 		# She enters "Use peacock feathers to make a fly" (Edith is very methodical)
